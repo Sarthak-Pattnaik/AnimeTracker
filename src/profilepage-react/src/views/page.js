848a22537eb76c588bdcafb4be24565dbdc1315f
Sarthak Pattnaik <psarthak@protonmail.com>
2025-01-19 16:01:58 +0530

    Added profile page

---
+++ src/profilepage-react/src/views/page.js
@@ -0,0 +1,89 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './page.css'
+
+const Page = (props) => {
+  return (
+    <div className="page-container1">
+      <Helmet>
+        <title>Page - exported project</title>
+        <meta property="og:title" content="Page - exported project" />
+      </Helmet>
+      <div className="page-wireframe1">
+        <img
+          src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/d12ab9e3-1710-4d4b-a98d-026771d7ff13/f06d4c3d-01d9-4f84-8cff-c6bac94ee752?org_if_sml=1699&amp;force_format=original"
+          alt="Rectangle23395"
+          className="page-rectangle2"
+        />
+        <span className="page-text1">ANIMETRACKER.COM</span>
+        <span className="page-text2">HOME</span>
+        <span className="page-text3">COMMUNITY</span>
+        <div className="page-searchbar">
+          <div className="page-statelayer1">
+            <div className="page-leadingicon">
+              <div className="page-container2">
+                <div className="page-statelayer2">
+                  <img
+                    src="/external/iconi332-cl5b.svg"
+                    alt="IconI332"
+                    className="page-icon1"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="page-content">
+              <span className="page-text4 M3bodylarge">Hinted search text</span>
+            </div>
+            <div className="page-trailing-elements">
+              <div className="page-frame1sttrailingicon">
+                <div className="page-container3">
+                  <div className="page-statelayer3">
+                    <img
+                      src="/external/iconi332-v9w.svg"
+                      alt="IconI332"
+                      className="page-icon2"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img
+          src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/d12ab9e3-1710-4d4b-a98d-026771d7ff13/1e882169-0eaf-4f05-9020-1b40311a90f5?org_if_sml=12191&amp;force_format=original"
+          alt="Rectangle13394"
+          className="page-rectangle1"
+        />
+        <img
+          src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/d12ab9e3-1710-4d4b-a98d-026771d7ff13/037f7d43-fe46-4bda-99c9-e4a986b098de?org_if_sml=1465&amp;force_format=original"
+          alt="Rectangle33397"
+          className="page-rectangle3"
+        />
+        <img
+          src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/d12ab9e3-1710-4d4b-a98d-026771d7ff13/d11a873d-3b1a-4cb3-aa85-1586b66c2e31?org_if_sml=12559&amp;force_format=original"
+          alt="Ellipse13396"
+          className="page-ellipse1"
+        />
+        <span className="page-text5 M3bodylarge">TRACKER SCORE</span>
+        <span className="page-text6 M3bodylarge">POSTS</span>
+        <img
+          src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/d12ab9e3-1710-4d4b-a98d-026771d7ff13/34a64224-6ef8-4364-917d-0399df333877?org_if_sml=12775&amp;force_format=original"
+          alt="Rectangle43401"
+          className="page-rectangle4"
+        />
+        <span className="page-text7 M3bodylarge">COMMUNITY JOINED</span>
+        <span className="page-text8 M3bodylarge">INTREST GROUPS</span>
+        <img
+          src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/d12ab9e3-1710-4d4b-a98d-026771d7ff13/825eb810-4a79-4856-9beb-32849c3e5410?org_if_sml=11984&amp;force_format=original"
+          alt="Rectangle52302"
+          className="page-rectangle5"
+        />
+        <span className="page-text9">PERSONAL ACCOUNT</span>
+      </div>
+    </div>
+  )
+}
+
+export default Page
---
+++ src/profilepage-react/src/views/tv1.js
@@ -0,0 +1,159 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './tv1.css'
+
+const TV1 = (props) => {
+  return (
+    <div className="tv1-container1">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="tv1tv1">
+        <img
+          alt="Rectangle7225"
+          src="/external/rectangle7225-oecc-200h.png"
+          className="tv1-rectangle7"
+        />
+        <span className="tv1-text10">ANIMETRACKER.COM</span>
+        <span className="tv1-text11">HOME</span>
+        <img
+          alt="Rectangle129"
+          src="/external/rectangle129-wi3m-300w.png"
+          className="tv1-rectangle1"
+        />
+        <img
+          alt="Ellipse1210"
+          src="/external/ellipse1210-f5rl-200h.png"
+          className="tv1-ellipse1"
+        />
+        <img
+          alt="Rectangle2211"
+          src="/external/rectangle2211-mny-300w.png"
+          className="tv1-rectangle2"
+        />
+        <img
+          alt="Rectangle3213"
+          src="/external/rectangle3213-mvh-400h.png"
+          className="tv1-rectangle3"
+        />
+        <img
+          alt="Rectangle4215"
+          src="/external/rectangle4215-oow7-300w.png"
+          className="tv1-rectangle4"
+        />
+        <img
+          alt="Rectangle5217"
+          src="/external/rectangle5217-ehi6-300h.png"
+          className="tv1-rectangle5"
+        />
+        <img
+          alt="Rectangle6221"
+          src="/external/rectangle6221-yypo-300h.png"
+          className="tv1-rectangle6"
+        />
+        <span className="tv1-text12">YOUR TRACKER SCORE</span>
+        <span className="tv1-text13">TRENDING</span>
+        <img
+          alt="istockphoto1495088043612x6121226"
+          src="/external/istockphoto1495088043612x6121226-urxh-200h.png"
+          className="tv1-istockphoto1495088043612x6121"
+        />
+        <img
+          alt="IMAGE4556651227"
+          src="/external/image4556651227-rv2m-200w.png"
+          className="tv1image4556651"
+        />
+        <img
+          alt="Rectangle8252"
+          src="/external/rectangle8252-90v-300w.png"
+          className="tv1-rectangle8"
+        />
+        <span className="tv1-text14">COMMUNITY</span>
+        <span className="tv1-text15">FRIENDS</span>
+        <img
+          alt="Rectangle92156"
+          src="/external/rectangle92156-46wj-200h.png"
+          className="tv1-rectangle9"
+        />
+        <img
+          alt="Upload2170"
+          src="/external/upload2170-yiqf.svg"
+          className="tv1-upload"
+        />
+        <span className="tv1-text16">POST NOW</span>
+        <img
+          alt="Rectangle102173"
+          src="/external/rectangle102173-srqb-300w.png"
+          className="tv1-rectangle10"
+        />
+        <span className="tv1-text17">DAILY TASKS</span>
+        <img
+          alt="Rectangle112175"
+          src="/external/rectangle112175-l7y-200h.png"
+          className="tv1-rectangle11"
+        />
+        <img
+          alt="Rectangle122176"
+          src="/external/rectangle122176-fgeg-200h.png"
+          className="tv1-rectangle12"
+        />
+        <img
+          alt="Rectangle132178"
+          src="/external/rectangle132178-vvr-200h.png"
+          className="tv1-rectangle13"
+        />
+        <img
+          alt="Rectangle142180"
+          src="/external/rectangle142180-ducq-200h.png"
+          className="tv1-rectangle14"
+        />
+        <img
+          alt="Rectangle152182"
+          src="/external/rectangle152182-xa2-200h.png"
+          className="tv1-rectangle15"
+        />
+        <span className="tv1-text18">SUGGESTIONS</span>
+        <img
+          alt="Rectangle16315"
+          src="/external/rectangle16315-rd1d-200h.png"
+          className="tv1-rectangle16"
+        />
+        <div className="tv1-searchbar">
+          <div className="tv1-statelayer1">
+            <div className="tv1-leadingicon">
+              <div className="tv1-container2">
+                <div className="tv1-statelayer2">
+                  <img
+                    alt="IconI321"
+                    src="/external/iconi321-nh0s.svg"
+                    className="tv1-icon1"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="tv1-content">
+              <span className="tv1-text19 M3bodylarge">Hinted search text</span>
+            </div>
+            <div className="tv1-trailing-elements">
+              <div className="tv1-frame1sttrailingicon">
+                <div className="tv1-container3">
+                  <div className="tv1-statelayer3">
+                    <img
+                      alt="IconI321"
+                      src="/external/iconi321-xl73.svg"
+                      className="tv1-icon2"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TV1
